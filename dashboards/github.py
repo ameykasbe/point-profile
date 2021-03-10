b28d95e7bcf4faf8bd0e9ef2fde443a431e78daf
Amey Kasbe <amey.kasbe@gmail.com>
2021-03-10 14:06:03 +0530

    User info updated. Templates updated.

--- dashboards/github.py
+++ dashboards/github.py
@@ -4,55 +4,109 @@ import json
 username = 'ameykasbe'
 token = 'a5a027f83aa8227d835a7d7fcd408ed99d8ab9b8'
 
+
 def get_response_text_dict(url):
-	"""Returns the response content of the GET request of a url in a dictionary format."""
-	response = requests.get(url, auth=(username, token))
-	try:
-		response.raise_for_status()
-	except Exception as e:
-		return e
-	response_text_dict = json.loads(response.text)
-	return response_text_dict
+    """Returns the response content of the GET request of a url in a dictionary format."""
+    response = requests.get(url)
+    # response = requests.get(url, auth=(username, token))
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        return e
+    response_text_dict = json.loads(response.text)
+    return response_text_dict
 
 
 def get_languages(user):
     """Returns a list of dictionaries of languages corresponding to different repositories of a user."""
+    url = "https://api.github.com/users/" + user + "/repos"
+    repos = get_response_text_dict(url)
+    if type(repos) == list:  # If no error occured.
+        languages_list = list()
+        for repo in repos:
+            languages_url = repo.get('languages_url')
+            languages = get_response_text_dict(languages_url)
+            languages_list.append(languages)
+        return languages_list
+    return repos  # If error occured, return the error.
 
-    # repos_languages = [{'JavaScript': 94325, 'CSS': 7750, 'HTML': 821}, {'Python': 10055, 'HTML': 4771, 'CSS': 274}, {'HTML': 3499, 'Python': 2712}, {'CSS': 202283, 'JavaScript': 100300, 'HTML': 40012, 'Python': 23442}, {'Python': 11083}, {'Python': 15386, 'HTML': 12253, 'CSS': 1125}, {'Python': 9788, 'HTML': 2983}, {'Python': 7717, 'HTML': 3384}]
+    # languages_list = [
+    #     {'JavaScript': 94325, 'CSS': 7750, 'HTML': 821},
+    #     {'Python': 10055, 'HTML': 4771, 'CSS': 274},
+    #     {'HTML': 3499, 'Python': 2712},
+    #     {'CSS': 202283, 'JavaScript': 100300, 'HTML': 40012, 'Python': 23442},
+    #     {'Python': 11083},
+    #     {'Python': 15386, 'HTML': 12253, 'CSS': 1125},
+    #     {'Python': 9788, 'HTML': 2983},
+    #     {'Python': 7717, 'HTML': 3384}
+    # ]
+
+
+def get_languages_data(user):
+    """Returns a dictionary with all the languages used by a user with key as the language and value as the number of projects."""
+    """Returns a dictionary with all the languages used by a user with key as the language and value as the percentage of code written."""
+
+    # repos_languages = get_languages(user)
+    # if type(repos_languages) == list:
+    #     projects_per_languages = dict()
+    #     languages_distribution = dict()
+    #     for repo in repos_languages:
+    #         for (language, bytes_lang) in repo.items():
+    #             if language in projects_per_languages:
+    #                 projects_per_languages[language] += 1
+    #             else:
+    #                 projects_per_languages[language] = 1
+
+    #             if language in languages_distribution:
+    #                 languages_distribution[language] += bytes_lang
+    #             else:
+    #                 languages_distribution[language] = bytes_lang
+    #     return projects_per_languages, languages_distribution
     # return repos_languages
 
-    # url = "https://api.github.com/users/" + user + "/repos"
-    # repos = get_response_text_dict(url)
-    # if type(repos) == list: # If no error occured.
-    #     languages_list = list()
-    #     for repo in repos:
-    #         languages_url = repo.get('languages_url')
-    #         languages = get_response_text_dict(languages_url)
-    #         languages_list.append(languages)
-    #     return languages_list
-    languages_list = [
-            {'JavaScript': 94325, 'CSS': 7750, 'HTML': 821}, 
-            {'Python': 10055, 'HTML': 4771, 'CSS': 274}, 
-            {'HTML': 3499, 'Python': 2712}, 
-            {'CSS': 202283, 'JavaScript': 100300, 'HTML': 40012, 'Python': 23442}, 
-            {'Python': 11083}, 
-            {'Python': 15386, 'HTML': 12253, 'CSS': 1125}, 
-            {'Python': 9788, 'HTML': 2983}, 
-            {'Python': 7717, 'HTML': 3384}
-        ]
-    return languages_list
-    return repos # If error occured, return the error.
+    projects_per_languages = {'JavaScript': 2,
+                              'CSS': 4, 'HTML': 7, 'Python': 7}
+    languages_distribution = {'JavaScript': 194625,
+                              'CSS': 211432, 'HTML': 67723, 'Python': 80183}
+    return projects_per_languages, languages_distribution
 
 
-def projects_per_languages(user):
-    """Returns a dictionary with all the languages used by a user with key as the language and value as the number of projects."""
-    repos_languages = get_languages(user)
-    languages = dict()
-    for repo in repos_languages:
-        for (language,bytes_lang) in repo.items():
-            if language in languages:
-                languages[language] += 1
-            else:
-                languages[language] = 1
-    return languages
-    # languages = {'JavaScript': 2, 'CSS': 4, 'HTML': 7, 'Python': 7}
+def get_user_info(user):
+    """Returns user information."""
+    # url = "https://api.github.com/users/" + user
+    # user_info_response = get_response_text_dict(url)
+    user_info_response = {
+        'login': 'anamayasarvate',
+        'id': 47415511,
+        'node_id': 'MDQ6VXNlcjQ3NDE1NTEx',
+        'avatar_url': 'https://avatars.githubusercontent.com/u/47415511?v=4',
+        'gravatar_id': '',
+        'url': 'https://api.github.com/users/anamayasarvate',
+        'html_url': 'https://github.com/anamayasarvate',
+        'followers_url': 'https://api.github.com/users/anamayasarvate/followers',
+        'following_url': 'https://api.github.com/users/anamayasarvate/following{/other_user}',
+        'gists_url': 'https://api.github.com/users/anamayasarvate/gists{/gist_id}',
+        'starred_url': 'https://api.github.com/users/anamayasarvate/starred{/owner}{/repo}',
+        'subscriptions_url': 'https://api.github.com/users/anamayasarvate/subscriptions',
+        'organizations_url': 'https://api.github.com/users/anamayasarvate/orgs',
+        'repos_url': 'https://api.github.com/users/anamayasarvate/repos',
+        'events_url': 'https://api.github.com/users/anamayasarvate/events{/privacy}',
+        'received_events_url': 'https://api.github.com/users/anamayasarvate/received_events',
+        'type': 'User',
+        'site_admin': False,
+        'name': 'Anamay Sarvate',
+        'company': None,
+        'blog': 'www.anamaysarvate.com',
+        'location': None,
+        'email': None,
+        'hireable': None,
+        'bio': 'Python/Django and MERN stack developer',
+        'twitter_username': None,
+        'public_repos': 12,
+        'public_gists': 0,
+        'followers': 0,
+        'following': 0,
+        'created_at': '2019-02-07T09:49:37Z',
+        'updated_at': '2021-03-09T15:06:51Z'}
+
+    return user_info_response
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 from . import github as gh
 
 
 def profile_analysis(request):
     user = "anamayasarvate"
-    projects_per_language = gh.projects_per_languages(user)    
-    languages = [key for key in projects_per_language]
-    num_projects = [value for key, value in projects_per_language.items()]
+    projects_per_languages, lang_distribution = gh.get_languages_data(
+        user)
 
+    # Projects per languages
+    languages = [key for key in projects_per_languages]
+    num_projects = [value for key, value in projects_per_languages.items()]
+
+    # Language distribution
+    languages_distribution = [key for key in lang_distribution]
+    bytes_per_language = [value for key,
+                          value in lang_distribution.items()]
+
+    # User info
+    user_info_response = gh.get_user_info(user)
+    user_info = dict()
+    user_info['name'] = user_info_response.get('name')
+    user_info['email'] = user_info_response.get('email')
+    user_info['company'] = user_info_response.get('company')
+    user_info['location'] = user_info_response.get('location')
+    user_info['github_id'] = '@' + user_info_response.get('login')
+    user_info['avatar_url'] = user_info_response.get('avatar_url')
+    user_info['html_url'] = user_info_response.get('html_url')
+    user_info['bio'] = user_info_response.get('bio')
+    created_at = user_info_response.get('created_at')
+    # Format = '2019-02-07T09:49:37Z'
+    created_at = datetime.datetime.fromisoformat(created_at[:-1])
+    # Format = datetime.datetime(2019, 2, 7, 9, 49, 37)
+    user_info['created_at'] = created_at.date()
+    # Format = datetime.datetime(2019, 2, 7)
     context = {
-        'languages' : languages,
+        'languages': languages,
         'num_projects': num_projects,
+        'languages_distribution': languages_distribution,
+        'bytes_per_language': bytes_per_language,
+        'user_info': user_info
     }
     return render(request, 'dashboards/profile-analysis.html', context)
